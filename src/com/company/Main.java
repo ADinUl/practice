1caa5f680689a4f4b9f3fb19b0e17cd325bfc34a
ADinUl <ata.din11@gmail.com>
2019-12-21 15:14:40 +0600

    first commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("Hello! How are u? , My name 's Dinash. What 's ur name?");
+        System.out.println("Hi! doing well, What about u? Oh, good I 'm Muha. Nice to meet u.");
+        System.out.println("What do u do, when do have some free time.git  ");
     }
 }
